cf643e0c36a6b77e0e3d27ca175636f8e90e94a1
Douglas A B Novato <douglasabnovato@gmail.com>
2022-04-26 11:35:30 -0300

    botão add tarefa

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function addCard() {
-  alert("oi");
+function addCard(elemento) {
+  const ulId = elemento.previousElementSibling.id;
+  const text = prompt("Qual é a tarefa?");
+  const board = document.getElementById(ulId);
+
+  const template = `
+    <li>
+        <p>${text}</p>
+        <p onclick="removeCard(this)" class="remove">x</p>
+    </li>
+  `;
+
+  board.innerHTML += template;
+
 }
+
+function removeCard() {
+    alert("removeCard");
+  }
+  
