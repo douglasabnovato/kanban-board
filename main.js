2978f214fa856dba8e9d16f39744386a0b485596
Douglas A B Novato <douglasabnovato@gmail.com>
2022-04-26 12:54:27 -0300

    arrasta e solta das tarefas no quadro

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* adicionar uma tarefa */
 function addCard(element) {
   const ulId = element.previousElementSibling.id;
   const text = prompt("Qual é a tarefa?");
@@ -13,10 +14,12 @@ function addCard(element) {
   board.innerHTML += template;
 }
 
+/* remover uma tarefa */
 function removeCard(element) {
   document.getElementById(element.parentElement.id).remove();
 }
 
+/* arrastar uma tarefa entre os quadros */
 function drag(event) {
   event.dataTransfer.setData("card", event.target.id);
 }
